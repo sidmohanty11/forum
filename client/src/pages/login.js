b6835c27e0be3e2d9ef1f3738accc79aab2d9a67
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-04-25 09:25:19 +0530

    register form + login form

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -5,53 +5,89 @@ import {
   Input,
   FormErrorMessage,
   Button,
+  Container,
+  Center,
   Box,
+  Heading,
+  Text,
 } from "@chakra-ui/react";
 import { Form, Field, Formik } from "formik";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  function validateName(value) {
-    let error;
-    if (!value) {
-      error = "Name is required";
-    } else if (value.toLowerCase() !== "naruto") {
-      error = "Jeez! You're not a fan 😱";
-    }
-    return error;
-  }
   return (
-    <Box>
-      <Formik
-        initialValues={{ name: "Sasuke" }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
-        }}
-      >
-        {(props) => (
-          <Form>
-            <Field name="name" validate={validateName}>
-              {({ field, form }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel htmlFor="name">First name</FormLabel>
-                  <Input {...field} id="name" placeholder="name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={props.isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
+    <Box
+      display={"flex"}
+      justifyContent="center"
+      alignItems={"center"}
+      height="100vh"
+    >
+      <Container boxShadow={"xl"} rounded={"md"} p={6}>
+        <Center>
+          <Heading py={4}>LOGIN | OUTR-Forums</Heading>
+        </Center>
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={(values, actions) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              actions.setSubmitting(false);
+            }, 1000);
+          }}
+        >
+          {(props) => (
+            <Form>
+              <Field name="email">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="email">
+                      Email
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="email"
+                      id="email"
+                      placeholder="Email"
+                    />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="password">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="password">
+                      Password
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="password"
+                      id="password"
+                      placeholder="Password"
+                    />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Text mt={4}>
+                Don't have an account yet? <Link to="/register">Register</Link>
+              </Text>
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
     </Box>
   );
 };
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,7 +1,144 @@
 import React from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+  Container,
+  Center,
+  Box,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { Form, Field, Formik } from "formik";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Box
+      display={"flex"}
+      justifyContent="center"
+      alignItems={"center"}
+      height="100vh"
+    >
+      <Container boxShadow={"xl"} rounded={"md"} p={6}>
+        <Center>
+          <Heading py={4}>REGISTER | OUTR-Forums</Heading>
+        </Center>
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={(values, actions) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              actions.setSubmitting(false);
+            }, 1000);
+          }}
+        >
+          {(props) => (
+            <Form>
+              <Field name="name">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="name">
+                      Name
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="text"
+                      id="name"
+                      placeholder="Name"
+                    />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="regNo">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="regNo">
+                      Registration Number
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="text"
+                      id="regNo"
+                      placeholder="Registration Number"
+                    />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="bio">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="bio">
+                      Bio
+                    </FormLabel>
+                    <Input {...field} type="text" id="bio" placeholder="Name" />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="email">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="email">
+                      Email
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="email"
+                      id="email"
+                      placeholder="Email"
+                    />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="password">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel mt={4} htmlFor="password">
+                      Password
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="password"
+                      id="password"
+                      placeholder="Password"
+                    />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Text mt={4}>
+                Already have an account? <Link to="/login">Login</Link>
+              </Text>
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </Box>
+  );
 };
 
 export default Register;
