ca9c153f76f61c15a4ae58552f75fa1fa0aef085
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-05-15 11:11:32 +0530

    small changes

--- server/dev.sh
+++ server/dev.sh
@@ -2,4 +2,5 @@
 
 npm i
 DATABASE_URL="postgres://postgres:postgres@postgres:5432/mydb?sslmode=disable?connect_timeout=300" npx prisma generate
+DATABASE_URL="postgres://postgres:postgres@postgres:5432/mydb?sslmode=disable?connect_timeout=300" npx prisma db push
 DATABASE_URL="postgres://postgres:postgres@postgres:5432/mydb?sslmode=disable?connect_timeout=300" npm run dev
